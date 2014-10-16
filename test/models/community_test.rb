d1d06cbd2732b7780519f404752ef9997fadb562
Charlotte Hill <charlotte@hillnetwork.com>
2014-10-15 18:41:46 -0700

    challenge 2

--- test/models/community_test.rb
+++ test/models/community_test.rb
@@ -4,4 +4,9 @@ class CommunityTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'create community with valid name' do
+       c = Community.new name: 'Good'
+       assert c.valid?       
+  end
+
 end
